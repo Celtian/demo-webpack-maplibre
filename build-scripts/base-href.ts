91cd87697d7a624f2d6bf8bcd00651fcc4ee5747
Dominik Hladík <dominik.hladik@seznam.cz>
2023-09-23 23:00:49 +0200

    fix(href): base href changed

---
+++ build-scripts/base-href.ts
@@ -0,0 +1,23 @@
+import { readFile, writeFile } from 'fs-extra';
+import { join } from 'path';
+
+const baseHref = process.argv[2] || '/';
+
+const filePath = join(__dirname, '..', 'dist', 'index.html');
+
+readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error reading file: ${err}`);
+    return;
+  }
+
+  const modifiedHTML = data.replace(/<base href=".*" \/>/, `<base href="${baseHref}" />`);
+
+  writeFile(filePath, modifiedHTML, 'utf8', (err) => {
+    if (err) {
+      console.error(`Error writing file: ${err}`);
+    } else {
+      console.log('base href replaced successfully!');
+    }
+  });
+});
